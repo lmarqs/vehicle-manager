689cdfbfc691be1ca13ff4d638034f05a9e98e96
lmarqs <marques.analista@gmail.com>
2020-02-12 20:09:42 -0300

    create api

---
+++ api/src/model/vehicle.ts
@@ -0,0 +1,37 @@
+import { Model } from "./model";
+
+export const VEHICLE = "Vehicle";
+
+export enum VehicleType {
+  BUS = "BUS",
+  TRUCK = "TRUCK",
+  CAR = "CAR",
+}
+
+export interface AbstractVehicle extends Model {
+  chassisNumber?: string;
+  chassisSeries?: string;
+  numberOfPassengers?: number;
+  type?: VehicleType;
+}
+
+export interface Bus extends AbstractVehicle {
+  numberOfPassengers: 42;
+  type: VehicleType.BUS;
+}
+
+export interface Truck extends AbstractVehicle {
+  numberOfPassengers: 42;
+  type: VehicleType.BUS;
+}
+
+export interface Car extends AbstractVehicle {
+  numberOfPassengers: 4;
+  type: VehicleType.CAR;
+}
+
+export type Vehicle =
+  | Bus
+  | Truck
+  | Car
+  ;
